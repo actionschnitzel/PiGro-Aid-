56c28c98860571296ca956e58003d4aa0d91ec90
Timo Westphal <67635013+actionschnitzel@users.noreply.github.com>
2020-10-24 20:26:17 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@ sudo chmod +x start.sh
 sudo chmod +x scripts/raspiconfiginstall.sh
 sudo chmod +x scripts/update.sh
 sudo chmod +x scripts/lxappearance.sh
-sudo cp pigro.desktop  ~/Desktop
+sudo cp pigro.desktop  /home/pi/Desktop
 sudo cp pigro.desktop /usr/share/applications/
