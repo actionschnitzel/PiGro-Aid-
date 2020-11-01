9168e1cea5ca9a7e1d4ef92f91fe91ac8ce24900
Timo Westphal <67635013+actionschnitzel@users.noreply.github.com>
2020-11-01 18:58:57 +0100

    Refreshed the code

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 from tkinter import *
 import os
 import sys
@@ -35,23 +34,23 @@ def button_action7():
     os.system ("lxterminal -e 'bash -c \"sudo nano /boot/config.txt; exec bash\"'")
     
 def button_action8():   
-    os.system ("lxterminal -e 'bash -c \"sudo neofetch; exec bash\"'")
+    os.system ("lxterminal -e 'bash -c \"neofetch; exec bash\"'")
     
 def button_action9():   
     os.system ("lxterminal -e 'bash -c \"sudo tasksel; exec bash\"'")
 
 def button_action10():   
-    os.system ("lxterminal -e 'bash -c \"sudo neofetch; exec bash\"'")
+    os.system ("lxterminal -e 'bash -c \"neofetch; exec bash\"'")
 #############################################################################
 #MAIN
     
 main = Tk()
 main.title("PiGro+Aid+")
-main.geometry("455x500")
+main.geometry("455x550")
 main['background']='grey10'
 
 photo = PhotoImage(file = r"/home/pi/PiGro-Aid-/icons/emblem-debian.png") 
-photoimage = photo.subsample(3,3) 
+photoimage = photo.subsample(2,2) 
 
 photo = PhotoImage(file = r"/home/pi/PiGro-Aid-/icons/rpi-logo.png") 
 photoimage2 = photo.subsample(2,2) 
@@ -60,7 +59,7 @@ photo = PhotoImage(file = r"/home/pi/PiGro-Aid-/icons/879px-Bleachbit_logo.svg.p
 photoimage3 = photo.subsample(30,25)
 
 photo = PhotoImage(file = r"/home/pi/PiGro-Aid-/icons/logo_pikiss_400.png") 
-photoimage4 = photo.subsample(15,15)
+photoimage4 = photo.subsample(13,13)
 
 photo = PhotoImage(file = r"/home/pi/PiGro-Aid-/icons/tux.png") 
 photoimage5 = photo.subsample(15,15) 
@@ -79,27 +78,27 @@ w['foreground']='white'
 
 
 
-button0 = Button(main,compound = LEFT, image = photoimage, text="Update your OS           ",foreground="white", command=button_action)
+button0 = Button(main, anchor="w", image = photoimage, text="Update",foreground="white", command=button_action, compound = LEFT)
 button0['background']='grey10'
-button1 = Button(main, text="Raspi-Config                ",foreground="white", command=button_action1, image = photoimage2, compound = LEFT)
+button1 = Button(main, anchor="w",compound = LEFT, image = photoimage, text="Raspi-Config",foreground="white", command=button_action1)
 button1['background']='grey10'
-button2 = Button(main, text ="Upgrade your OS         ", foreground="white", command=button_action2, image = photoimage, compound = LEFT)
+button2 = Button(main, anchor="w",compound = LEFT, image = photoimage, text ="Upgrade", foreground="white", command=button_action2)
 button2['background']='grey10'
-button3 = Button(main,text="Change PiXEL Theme  ",foreground="white", command=button_action3, image = photoimage2, compound = LEFT)
+button3 = Button(main, anchor="w",compound = LEFT, image = photoimage, text="Change PiXEL Theme",foreground="white", command=button_action3)
 button3['background']='grey10'
-button4 = Button(main,text="Edit Source                  ",foreground="white", command=button_action4, image = photoimage ,compound = LEFT)
+button4 = Button(main, anchor="w",compound = LEFT,text="Edit Source",foreground="white", command=button_action4, image = photoimage)
 button4['background']='grey10'
-button5 = Button(main,text="Install Pikiss                 ",foreground="white", command=button_action5, image = photoimage4 ,compound = LEFT)
+button5 = Button(main, anchor="w",compound = LEFT,text="Install Pikiss",foreground="white", command=button_action5, image = photoimage4)
 button5['background']='grey10'
-button6 = Button(main,text="Install Compiz             ",foreground="white", command=button_action6, image = photoimage ,compound = LEFT)
+button6 = Button(main, anchor="w",compound = LEFT,text="Install Compiz",foreground="white", command=button_action6, image = photoimage)
 button6['background']='grey10'
-button7 = Button(main,text="Nano Config.txt           ",foreground="white", command=button_action7, image = photoimage2 ,compound = LEFT)
+button7 = Button(main, anchor="w",compound = LEFT,text="Nano Config.txt",foreground="white", command=button_action7, image = photoimage)
 button7['background']='grey10'
-button8 = Button(main,text="$sudo rm -rf/(Don't klick)    ",foreground="white", command=button_action8, image = photoimage5 ,compound = LEFT)
+button8 = Button(main, anchor="w",compound = LEFT,text="Neofetch",foreground="white", command=button_action8, image = photoimage5)
 button8['background']='grey10'
-button9 = Button(main,text="Tasksel                       ",foreground="white", command=button_action9, image = photoimage ,compound = LEFT)
+button9 = Button(main, anchor="w",compound = LEFT,text="Tasksel",foreground="white", command=button_action9, image = photoimage)
 button9['background']='grey10'
-button10 = Button(main,text="NeoFetch               ",foreground="white", command=button_action10, image = photoimage ,compound = LEFT)
+button10 = Button(main, anchor="w",compound = LEFT,text="Nothing Jet",foreground="white", command=button_action10, image = photoimage)
 button10['background']='grey10'
 #####
 #newwin
@@ -108,19 +107,17 @@ button10['background']='grey10'
 
 
 ####mainwin
-button0.place(x = 0, y = 0, width=200, height=30)
-button2.place(x = 0, y = 30, width=200, height=30)
-button1.place(x = 0, y = 60, width=200, height=30)
-button3.place(x = 0, y = 90, width=200, height=30)
-button4.place(x = 0, y = 120, width=200, height=30)
-button5.place(x = 0, y = 150, width=200, height=30)
-button6.place(x = 0, y = 180, width=200, height=30)
-button7.place(x = 0, y = 210, width=200, height=30)
-button8.place(x = 0, y = 240, width=200, height=30)
-button9.place(x = 0, y = 290, width=200, height=30)
-button10.place(x = 0, y = 320, width=200, height=30)
+button0.place(x = 0, y = 0, width=200, height=50)
+button2.place(x = 0, y = 50, width=200, height=50)
+button1.place(x = 0, y = 100, width=200, height=50)
+button3.place(x = 0, y = 150, width=200, height=50)
+button4.place(x = 0, y = 200, width=200, height=50)
+button5.place(x = 0, y = 250, width=200, height=50)
+button6.place(x = 0, y = 300, width=200, height=50)
+button7.place(x = 0, y = 350, width=200, height=50)
+button8.place(x = 0, y = 400, width=200, height=50)
+button9.place(x = 0, y = 450, width=200, height=50)
+button10.place(x = 0, y = 500, width=200, height=50)
 
 ###################################################################################################
 main.mainloop()
-
-
