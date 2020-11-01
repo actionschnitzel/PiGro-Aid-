f5aae498077ceb3522880778443bb6a1f6f58136
Timo Westphal <67635013+actionschnitzel@users.noreply.github.com>
2020-11-01 16:27:55 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+sudo apt-get update
+sudo apt-get install neofetch
 sudo chmod +x start.sh
 sudo chmod +x scripts/raspiconfiginstall.sh
 sudo chmod +x scripts/update.sh
@@ -6,3 +8,4 @@ sudo chmod +x scripts/rmlxde.sh
 sudo chmod +x scripts/terminal.sh
 sudo cp pigro.desktop  /home/pi/Desktop
 sudo cp pigro.desktop /usr/share/applications/
+sudo apt-get install neofetch
