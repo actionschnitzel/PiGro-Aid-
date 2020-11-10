2df0dac7e78cd443a7798a8533f12b634b6851b9
Timo Westphal <67635013+actionschnitzel@users.noreply.github.com>
2020-11-11 00:48:23 +0100

    :metal: New Look
    First GUI looked to 2005ish ^^ ...... think this one is better ;-)

--- main.py
+++ main.py
@@ -8,9 +8,13 @@ from tkinter import messagebox
 ###########################################################################
 #DEFINITIONS
 
+
+
+
+
+
 def button_action():
     popen("lxterminal -e 'bash -c \"sudo apt-get update; exec bash\"'")
-   
     
 def button_action1():
     popen("lxterminal -e 'bash -c \"sudo raspi-config; exec bash\"'")    
@@ -44,101 +48,81 @@ def button_action10():
     
 def button_action11():   
     popen ("lxterminal -e 'bash -c \"raspistill -o image.jpg; exec bash\"'")
-    
-#############################################################################
-#MAIN
-    
-main = Tk()
-main.title("PiGro+Aid+")
-main.geometry("620x510")
-main['background']='grey10'
-##############################################################################
-photo = PhotoImage(file = r"/home/pi/PiGro-Aid-/icons/emblem-debian.png") 
-photoimage = photo.subsample(2,2) 
+        
+def action_get_info_dialog():
+    m_text = "\
+************************\n\
+Autor: Timo Westphal\n\
+Date: 11.11.2020\n\
+Version: 2.0\n\
+************************"
+    messagebox.showinfo(message=m_text, title = "Infos")
+        
 
-photo = PhotoImage(file = r"/home/pi/PiGro-Aid-/icons/picam.png") 
-photoimage2 = photo.subsample(5,4) 
+        
+        
+fenster = Tk()
+fenster.title("PiGro - Don't Cry Little Pi")
+fenster.geometry("400x250")
+fenster['background']='grey10'
 
-photo = PhotoImage(file = r"/home/pi/PiGro-Aid-/icons/879px-Bleachbit_logo.svg.png") 
-photoimage3 = photo.subsample(30,25)
 
-photo = PhotoImage(file = r"/home/pi/PiGro-Aid-/icons/logo_pikiss_400.png") 
-photoimage4 = photo.subsample(12,12)
 
-photo = PhotoImage(file = r"/home/pi/DEV/PiGro-Aid-/icons/neofetch.png") 
-photoimage5 = photo.subsample(7,5) 
+# Menüleiste 
+menuleiste = Menu(fenster)
+menuleiste['background']='snow'
+# Menüeinträge
+system_menu = Menu(menuleiste, tearoff=0)
+appearance_menu = Menu(menuleiste, tearoff=0)
+tools_menu = Menu(menuleiste, tearoff=0)
+help_menu = Menu(menuleiste, tearoff=0)
 
-i=Image.open('/home/pi/DEV/PiGro-Aid-/raspi-aid.png')
+# System
+# 
+system_menu.add_command(label="Update", command=button_action)
+system_menu.add_command(label="Upgrade", command=button_action2)
+system_menu.add_separator() 
+system_menu.add_command(label="Raspi-Config", command=button_action1)
+system_menu.add_command(label="Edit Source List", command=button_action4)
+system_menu.add_command(label="Nano Config.txt", command=button_action7)
+system_menu['background']='snow'
+#system_menu.add_command(label="Exit", command=fenster.quit)
+#
+#Appear
+appearance_menu.add_command(label="LXAppearance", command=button_action3)
+appearance_menu.add_command(label="Compiz", command=button_action6)
+appearance_menu.add_separator() 
+appearance_menu.add_command(label="Tasksel", command=button_action9)
+appearance_menu.add_command(label="Change Desktop Env.", command=button_action10)
+appearance_menu['background']='snow'
+#
+#
+#Tools
+tools_menu.add_command(label="Install/Start PiKiss", command=button_action5)
+tools_menu.add_command(label="NeoFetch", command=button_action8)
+tools_menu.add_separator() 
+tools_menu.add_command(label="Take A Photo", command=button_action11)
+tools_menu['background']='snow'
+#
+#
+#Help ....I need somebody... HELP!
+help_menu.add_command(label="Info!", command=action_get_info_dialog)
+help_menu['background']='snow'
+# 
+# "Drop-Down-Menü"
+menuleiste.add_cascade(label="System", menu=system_menu)
+menuleiste.add_cascade(label="Appearance", menu=appearance_menu)
+menuleiste.add_cascade(label="Tools", menu=tools_menu)
+menuleiste.add_cascade(label="Help", menu=help_menu)
+
+i=Image.open('/home/pi/PiGro-Aid-/raspi-aid.png')
 p=ImageTk.PhotoImage(i)
-l=Label(main,image = p)
+l=Label(fenster,image = p)
 l.image = p
-l.place(x=100, y=260)
 l['background']='grey10'
-#########################################################################
-sys_label = Label(main, text="System", fg="white")
-sys_label.configure(font=("Impact", 16,"italic"))
-sys_label['background']='grey10'
+l.grid(row=0, column=1)
 
-#######
-look_label = Label(main, text="Appearance", fg="white")
-look_label.configure(font=("Impact", 16,"italic"))
-look_label['background']='grey10'
-#######
-tool_label = Label(main, text="Tools", fg="white")
-tool_label.configure(font=("Impact", 16,"italic"))
-tool_label['background']='grey10'
+# 
+fenster.config(menu=menuleiste)          
 
-
-########################################################################
-button0 = Button(main, anchor="w", image = photoimage, text="Update",foreground="white", command=button_action, compound = LEFT,width=170)
-button0['background']='grey10'
-button1 = Button(main, anchor="w",compound = LEFT, image = photoimage, text="Raspi-Config",foreground="white", command=button_action1,width=170)
-button1['background']='grey10'
-button2 = Button(main, anchor="w",compound = LEFT, image = photoimage, text ="Upgrade", foreground="white", command=button_action2,width=170)
-button2['background']='grey10'
-button3 = Button(main, anchor="w",compound = LEFT, image = photoimage, text="Change PiXEL Theme",foreground="white", command=button_action3,width=170)
-button3['background']='grey10'
-button4 = Button(main, anchor="w",compound = LEFT,text="Edit Source",foreground="white", command=button_action4, image = photoimage,width=170)
-button4['background']='grey10'
-button5 = Button(main, anchor="w",compound = LEFT,text="Install Pikiss",foreground="white", command=button_action5, image = photoimage4,width=170)
-button5['background']='grey10'
-button6 = Button(main, anchor="w",compound = LEFT,text="Install Compiz",foreground="white", command=button_action6, image = photoimage,width=170)
-button6['background']='grey10'
-button7 = Button(main, anchor="w",compound = LEFT,text="Nano Config.txt",foreground="white", command=button_action7, image = photoimage,width=170)
-button7['background']='grey10'
-button8 = Button(main, anchor="w",compound = LEFT,text="Neofetch",foreground="white", command=button_action8, image = photoimage,width=170)
-button8['background']='grey10'
-button9 = Button(main, anchor="w",compound = LEFT,text="Tasksel",foreground="white", command=button_action9, image = photoimage,width=170)
-button9['background']='grey10'
-button10 = Button(main, anchor="w",compound = LEFT,text="Change Desktop \nEnvironment",foreground="white", command=button_action10, image = photoimage,width=170)
-button10['background']='grey10'
-button11 = Button(main, anchor="w",compound = LEFT,text="Take A Photo",foreground="white", command=button_action11, image = photoimage2,width=170)
-button11['background']='grey10'
-#####
-#newwin
-
-
-
-
-####mainwin
-sys_label.grid(row=0, column=1)
-look_label.grid(row=0, column=2)
-tool_label.grid(row=0, column=3)
-button0.grid(padx=5, row=1, column=1)
-button2.grid(row=2, column=1)
-button1.grid(row=3, column=1)
-button3.grid(row=1, column=2)
-button4.grid(row=4, column=1)
-button5.grid(padx=5,row=1, column=3)
-button6.grid(row=2, column=2)
-button7.grid(row=6, column=1)
-button8.grid(row=2, column=3)
-button9.grid(row=4, column=2)
-button10.grid( row=3, column=2)
-button11.grid(row=3, column=3)
-
-###################################################################################################
-main.mainloop()
-
-
-
+fenster.mainloop()
