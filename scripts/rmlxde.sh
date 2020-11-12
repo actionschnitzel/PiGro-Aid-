ef9d5af25993254114940ba59419ab4dd5e548bb
Timo Westphal <67635013+actionschnitzel@users.noreply.github.com>
2020-11-12 23:32:54 +0100

    Delete rmlxde.sh

--- scripts/rmlxde.sh
+++
@@ -1,8 +0,0 @@
-#Remove LXDE FULL
-
-sudo apt remove lxde* --purge
-sudo apt remove pipanel --purge
-sudo apt remove lxpanel --purge
-sudo apt remove openbox* --purge
-sudo apt remove obconf --purge
-sudo apt autoremove
