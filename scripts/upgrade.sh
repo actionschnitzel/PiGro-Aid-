eb26baf897f04e9955595df958914dd64c99868e
Timo Westphal <67635013+actionschnitzel@users.noreply.github.com>
2020-11-15 03:26:07 +0100

    more shell

---
+++ scripts/upgrade.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo apt-get update -y && sudo apt-get full-upgrade -y && sudo apt-get dist-upgrade -y && sudo apt autoremove -y && sudo apt autoclean
+
+read -p "Done! C YA!......Press Enter"
