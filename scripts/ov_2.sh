71aaf28fbad0864e563014b605351f85953a612e
Timo Westphal <67635013+actionschnitzel@users.noreply.github.com>
2021-03-08 13:33:36 +0100

    Update ov_2.sh

--- scripts/ov_2.sh
+++ scripts/ov_2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo cp /boot/config.txt{,.bak} && sudo sh -c 'echo "\narm_freq=2147\ngpu_freq=750\nover_voltage=6\ndisable_splash=1\nforce_turbo=1" >> /boot/config.txt'
+sudo cp /boot/config.txt{,.bak} && sudo sh -c 'echo "\narm_freq=2147\ngpu_freq=750\nover_voltage=8\ndisable_splash=1\nforce_turbo=1" >> /boot/config.txt'
 
 read -p "Done! You should reboot, C YA!......Press Enter"
