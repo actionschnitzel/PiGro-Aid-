58a989d5dfcf65b6fe23cb53b9b54c3ce563a85a
Timo Westphal <67635013+actionschnitzel@users.noreply.github.com>
2021-06-18 14:48:23 +0200

    Update 5.5

---
+++ scripts/lutris.sh
@@ -0,0 +1,4 @@
+echo "deb http://download.opensuse.org/repositories/home:/strycore/Debian_10/ ./" | sudo tee /etc/apt/sources.list.d/lutris.list
+wget -q https://download.opensuse.org/repositories/home:/strycore/Debian_10/Release.key -O- | sudo apt-key add -
+sudo apt update
+sudo apt install lutris
