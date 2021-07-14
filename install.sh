a12f3f366fff60dc740926eb62a706be05f7b21f
Timo Westphal <67635013+actionschnitzel@users.noreply.github.com>
2021-07-14 13:15:09 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -40,6 +40,7 @@ echo 'Now I install dependencies'
 
 sudo apt-get install xterm -y
 sudo apt-get install python3-pil python3-pil.imagetk -y
+sudo apt install python3-pip -y
 pip3 install playsound
 
 sudo chmod +x start.sh
