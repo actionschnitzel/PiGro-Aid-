3b082a04d88bd1a5b3f98bc3bcd7e7d825981865
Timo Westphal <actionschnitzel@live.com>
2022-06-05 17:40:19 +0200

    June RC1

--- install.sh
+++ install.sh
@@ -24,11 +24,11 @@ if [ -d "$HOME/PiGro-Aid-" ]
 then
     printf "${YELLOW}[UPDATE]${NC}I will install the newest version.\n\n" 
     rm -rf $HOME/PiGro-Aid-
-    git clone -b big_update https://github.com/actionschnitzel/PiGro-Aid-.git
+    git clone https://github.com/actionschnitzel/PiGro-Aid-.git
     cd PiGro-Aid-
 else
     printf "${YELLOW}[NEW INSTALL]${NC}I will now install PiGro\n\n"
-    git clone -b big_update https://github.com/actionschnitzel/PiGro-Aid-.git
+    git clone https://github.com/actionschnitzel/PiGro-Aid-.git
     cd PiGro-Aid-
 fi
 
