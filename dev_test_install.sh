4d5f77bfbccc8e04b3861eb07398e36ed8305a9a
Actionschnitzel <67635013+actionschnitzel@users.noreply.github.com>
2022-11-19 07:32:47 +0100

    Update dev_test_install.sh

--- dev_test_install.sh
+++ dev_test_install.sh
@@ -1,19 +1,24 @@
-echo "
-
-██████╗░██╗░██████╗░██████╗░░█████╗░░
-██╔══██╗██║██╔════╝░██╔══██╗██╔══██╗░
-██████╔╝██║██║░░██╗░██████╔╝██║░░██║░
-██╔═══╝░██║██║░░╚██╗██╔══██╗██║░░██║░
-██║░░░░░██║╚██████╔╝██║░░██║╚█████╔╝░
-╚═╝░░░░░╚═╝░╚═════╝░╚═╝░░╚═╝░╚════╝░░
-         🅹🆄🆂🆃 🅲🅻🅸🅲🅺 🅸🆃"
-
 YELLOW='\033[0;33m'
 GREEN='\033[0;32m'
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
-printf "${GREEN}I now install dependencies${NC}\n\n"
+printf "${YELLOW}██████╗ ██╗ ██████╗ ██████╗  █████╗  
+██╔══██╗██║██╔════╝ ██╔══██╗██╔══██╗ 
+██████╔╝██║██║  ██╗ ██████╔╝██║  ██║ 
+██╔═══╝ ██║██║  ╚██╗██╔══██╗██║  ██║ 
+██║     ██║╚██████╔╝██║  ██║╚█████╔╝ 
+╚═╝     ╚═╝ ╚═════╝ ╚═╝  ╚═╝ ╚════╝  
+${NC}"
+printf "${RED}         🅹🆄🆂🆃 🅲🅻🅸🅲🅺 🅸🆃${NC}\n\n"
+printf "Many tanks for using PiGro - Just Click I!\nHave Fun :-)"
+
+printf "${GREEN}\n\n🅸 🅽🅾🆆 🅸🅽🆂🆃🅰🅻🅻 🅳🅴🅿🅴🅽🅳🅴🅽🅲🅸🅴🆂.🅷🅰🅽🅶 🅾🅽...${NC}\n\n"
+
+printf "${YELLOW}xterm\npython3-pil\npython3-pil.imagetk\npython3-pip${NC}\n"
+printf "${YELLOW}python3-dev\npython3-distro\npython3-psutil\nlibnotify-bin\nlolcat${NC}\n\n"
+sleep 2
+printf "${RED}🅶🅾!${NC}\n\n"
 
 sudo apt-get install -y xterm python3-pil python3-pil.imagetk python3-pip python3-dev python3-distro python3-psutil libnotify-bin mpg123 lolcat
 
@@ -26,12 +31,12 @@ cd
 
 if [ -d "$HOME/PiGro-Aid-" ] 
 then
-    printf "${YELLOW}[UPDATE]${NC}I will install the newest version.\n\n" 
+    printf "${YELLOW}[🆄🅿🅳🅰🆃🅴]${NC}I will install the newest version.\n\n" 
     rm -rf $HOME/PiGro-Aid-
     git clone -b dev https://github.com/actionschnitzel/PiGro-Aid-.git
     cd PiGro-Aid-
 else
-    printf "${YELLOW}[NEW INSTALL]${NC}I will now install PiGro\n\n"
+    printf "${YELLOW}[🅽🅴🆆 🅸🅽🆂🆃🅰🅻🅻]${NC}I will now install PiGro\n\n"
     git clone -b dev https://github.com/actionschnitzel/PiGro-Aid-.git
     cd PiGro-Aid-
 fi
@@ -77,11 +82,12 @@ clear
 
 echo "
 
-██████╗░██╗░██████╗░██████╗░░█████╗░░
-██╔══██╗██║██╔════╝░██╔══██╗██╔══██╗░
-██████╔╝██║██║░░██╗░██████╔╝██║░░██║░
-██╔═══╝░██║██║░░╚██╗██╔══██╗██║░░██║░
-██║░░░░░██║╚██████╔╝██║░░██║╚█████╔╝░
-╚═╝░░░░░╚═╝░╚═════╝░╚═╝░░╚═╝░╚════╝░░
-         🆃🅷🅴 🆃🅰🆂🅺 🅸🆂 🅲🅾🅼🅿🅻🅴🆃🅴"
-printf "\n${GREEN}You can close this window now\n${NC}"
+printf "${NC}██████╗ ██╗ ██████╗ ██████╗  █████╗  
+██╔══██╗██║██╔════╝ ██╔══██╗██╔══██╗ 
+██████╔╝██║██║  ██╗ ██████╔╝██║  ██║ 
+██╔═══╝ ██║██║  ╚██╗██╔══██╗██║  ██║ 
+██║     ██║╚██████╔╝██║  ██║╚█████╔╝ 
+╚═╝     ╚═╝ ╚═════╝ ╚═╝  ╚═╝ ╚════╝  
+${NC}"
+printf "${RED}         🆃🅷🅴 🆃🅰🆂🅺 🅸🆂 🅲🅾🅼🅿🅻🅴🆃🅴${NC}\n\n"
+printf "${GREEN}You can close this window now\n${NC}"
