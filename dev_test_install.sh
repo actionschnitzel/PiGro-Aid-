3712984cdda42bb35b04cf592d56b53054de3740
Actionschnitzel <67635013+actionschnitzel@users.noreply.github.com>
2023-10-27 18:50:08 +0200

    Update dev_test_install.sh
    Added wmctrl

--- dev_test_install.sh
+++ dev_test_install.sh
@@ -25,7 +25,7 @@ ${NC}\n\n"
 echo  -e "\033[33;5mInstalling...\033[0m"
 echo "xterm python3-pil python3-pil.imagetk python3-pip python3-psutil python3-distro python3-bs4 python3-dev python3-requests mpg123 lolcat wmctrl"
 > /dev/null 2>&1 sudo apt-get update
-> /dev/null 2>&1 sudo apt-get install xterm python3-pil python3-pil.imagetk python3-pip python3-psutil python3-distro python3-bs4 python3-dev python3-requests mpg123 lolcat -y 
+> /dev/null 2>&1 sudo apt-get install xterm python3-pil python3-pil.imagetk python3-pip python3-psutil python3-distro python3-bs4 python3-dev python3-requests mpg123 lolcat wmctrl -y 
 
 
 
